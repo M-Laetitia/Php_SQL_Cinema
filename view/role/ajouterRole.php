38584930eae03a1802fdcf35c7c947a279052240
M-Laetitia <molin.laetitia@gmail.com>
2023-09-19 11:27:50 +0200

    Role form added

---
+++ view/role/ajouterRole.php
@@ -0,0 +1,28 @@
+<?php ob_start(); ?>
+
+
+    <form enctype="multipart/form-data" action="index.php?action=ajouterRole" method="post">
+        
+        <h3>Add a movie</h3>
+        
+        <div class="form-input">
+            <label for="name_role">Role :</label>
+            <input type="text" placeholder="Role name" name="name_role" id="name_role">
+        </div>
+
+        
+        <div class="form-input">
+            <input type="submit" class="submit" name="submitRole" id="submitRole">
+
+        </div>
+    </form>
+
+
+<?php
+
+$titre = "Role";
+$titre_secondaire = "Add role";
+$contenu = ob_get_clean();
+require "view/template.php";
+
+?>
