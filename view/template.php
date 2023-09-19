66484b1e906f9d97e0bd5b4eea4c66375c3fc761
M-Laetitia <molin.laetitia@gmail.com>
2023-09-19 22:42:11 +0200

    hyperlinks to form pages added

--- view/template.php
+++ view/template.php
@@ -16,6 +16,18 @@
                 <li><a href="index.php?action=listRealisateurs"> Director</a></li>
                 <li><a href="index.php?action=listGenres"> Genre</a></li>
                 <li><a href="index.php?action=listRoles"> Role</a></li>
+                <li>
+                    <select onchange="location = this.value;">
+                        <option>--ADD INFOS</option>
+                        <option value="index.php?action=getAjouterFilm">ADD MOVIES</option>
+                        <option value="index.php?action=getAjouterGenre">ADD GENRES</option>
+                        <option value="index.php?action=getAjouterRole">ADD ROLES</option>
+                        <option value="index.php?action=getAjouterActeur">ADD ACTORS</option>
+                        <option value="index.php?action=getAjouterRealisateur">ADD DIRECTORS</option>
+                        <option value="index.php?action=getAddCasting">ADD CASTING</option>
+                    </select>
+                </li>
+                
             </ul>
         </nav>
     </header>
