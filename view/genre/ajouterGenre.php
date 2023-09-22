09a247ac9121a5c7b635f0e42882740023d37dc7
M-Laetitia <molin.laetitia@gmail.com>
2023-09-22 17:40:53 +0200

    feat: Enhance and expand PHP form with new options

--- view/genre/ajouterGenre.php
+++ view/genre/ajouterGenre.php
@@ -1,21 +1,40 @@
 <?php ob_start(); ?>
 
 
-    <form enctype="multipart/form-data" action="index.php?action=ajouterGenre" method="post">
-        
-        <h3>Add a genre</h3>
-        
-        <div class="form-input">
-            <label for="label_genre">Genre :</label>
-            <input type="text" placeholder="Genre" name="label_genre" id="label_genre">
+    <div class="container_form">
+        <div class="left_section">
+            <h1>Let's Build a <br> <span class="text-colored">Cinematic Treasure <br> Trove</span>  together.</h1>
         </div>
 
-        
-        <div class="form-input">
-            <input type="submit" class="submit" name="submitGenre" id="submitGenre">
+        <div class="form_section">
+            <h2>Share Your Movie Wisdom</h2>
+            <h3>Add a Genre</h3>
+            <div class="form_info">
 
+                <!-- The enctype attribute specifies how the form-data should be encoded when submitting it to the server. 
+                This value is necessary if the user will upload a file through the form-->
+                <form enctype="multipart/form-data" action="index.php?action=ajouterGenre" method="post">
+                
+                    <div class="info_input">
+
+                        <div class="form-input">
+                            <label for="label_genre"></label>
+                            <input type="text" placeholder="Genre*" name="label_genre" id="label_genre" required>
+                        </div>
+
+        
+                    </div>
+
+                    <div class="btn-submit">
+                    <input type="submit" class="submit" name="submitGenre" id="submitGenre">
+                    </div>
+
+                </form>
+            
+            </div>
         </div>
-    </form>
+    </div>
+
 
 
 <?php
--- view/role/ajouterRole.php
+++ view/role/ajouterRole.php
@@ -1,21 +1,49 @@
 <?php ob_start(); ?>
 
 
-    <form enctype="multipart/form-data" action="index.php?action=ajouterRole" method="post">
+
+<div class="container_form">
+    <div class="left_section">
+        <h1>Let's Build a <br> <span class="text-colored">Cinematic Treasure <br> Trove</span>  together.</h1>
+    </div>
+
+    <div class="form_section">
+        <h2>Share Your Movie Wisdom</h2>
+        <h3>Add a Role</h3>
+        <div class="form_info">
+
+            <!-- The enctype attribute specifies how the form-data should be encoded when submitting it to the server. 
+            This value is necessary if the user will upload a file through the form-->
+            <form enctype="multipart/form-data" action="index.php?action=ajouterRole" method="post">
+            
+                <div class="info_input">
+
+                    <div class="form-input">
+                        <label for="name_role"></label>
+                        <input type="text" placeholder="Role name*" name="name_role" id="name_role" required>
+                    </div>
+    
+                </div>
+
+                <div class="btn-submit">
+                        <input type="submit" class="submit" name="submitRole" id="submitRole">
+                </div>
+
+            </form>
         
-        <h3>Add a role</h3>
-        
-        <div class="form-input">
-            <label for="name_role">Role :</label>
-            <input type="text" placeholder="Role name" name="name_role" id="name_role">
         </div>
+    </div>
+</div>
+
+
+
+
+
+    
+        
 
         
-        <div class="form-input">
-            <input type="submit" class="submit" name="submitRole" id="submitRole">
-
-        </div>
-    </form>
+    
 
 
 <?php
@@ -26,3 +54,4 @@ $contenu = ob_get_clean();
 require "view/template.php";
 
 ?>
+
