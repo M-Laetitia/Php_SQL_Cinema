60111e58f6016b8357f61de7db670dc3d33d2903
M-Laetitia <molin.laetitia@gmail.com>
2023-09-23 19:37:59 +0200

    style : improve the design of the page

--- view/role/updateRole.php
+++ view/role/updateRole.php
@@ -3,28 +3,37 @@ ob_start();
 $role = $requeteRole->fetch(); ?>
 
 
+<div class="container_form">
+    <div class="left_section">
+        <h1>Let's Build a <br> <span class="text-colored">Cinematic Treasure <br> Trove</span>  together.</h1>
+    </div>
 
-<div>
+    <div class="form_section">
+        <h2>Share Your Movie Wisdom</h2>
+        <h3>Update : <?=$role["name_role"]?></h3>
+        <div class="form_info">
 
 
-<form enctype="multipart/form-data" action="index.php?action=updateRole&id=<?=$role["id_role"]?>" method="post">
+            <form enctype="multipart/form-data" action="index.php?action=updateRole&id=<?=$role["id_role"]?>" method="post">
+            
+                <div class="info_input">
+
+                    <div class="form-input">
+                        <label for="name_role"></label>
+                        <input type="text" placeholder="Role name*" name="name_role" id="name_role" value="<?= $role["name_role"] ?>" required>
+                    </div>
+    
+                </div>
+
+                <div class="btn-submit">
+                        <input type="submit" class="submit" name="updateRole" id="submitRole" value="Update">
+                </div>
+
+            </form>
         
-        <h3>Update this role</h3>
-        
-        <div class="form-input">
-            <label for="name_role">Role :</label>
-            <input type="text" placeholder="Role name" name="name_role" value="<?= $role["name_role"] ?>" >
         </div>
-
-        
-        <div class="form-input">
-            <input type="submit" class="submit" name="updateRole" id="updateRole">
-
-        </div>
-</form>
-
+    </div>
 </div>
-
 
 
 
