b816454ce5995d378d7142ec0fedebf68e5972bc
M-Laetitia <molin.laetitia@gmail.com>
2023-09-23 19:35:34 +0200

    style : improve the design of the page

--- view/genre/updateGenre.php
+++ view/genre/updateGenre.php
@@ -3,23 +3,44 @@ ob_start();
 $genre = $requeteGenre->fetch(); ?>
 
 
+<div class="container_form">
+        <div class="left_section">
+            <h1>Let's Build a <br> <span class="text-colored">Cinematic Treasure <br> Trove</span>  together.</h1>
+        </div>
+
+        <div class="form_section">
+            <h2>Share Your Movie Wisdom</h2>
+            <h3>Update : <?=$genre["label_genre"]?> </h3>
+            <div class="form_info">
+
+                <form action="index.php?action=updateGenre&id=<?=$genre["id_genre"]?>" method="POST">
+                
+                    <div class="info_input">
+
+                        <div class="form-input">
+                            <label for="label_genre"></label>
+                            <input type="text" placeholder="Genre*" name="label_genre" id="label_genre" value="<?= $genre["label_genre"] ?>" required>
+                        </div>
+
+        
+                    </div>
+
+                    <div class="btn-submit">
+                    <input type="submit" class="submit" name="updateGenre"  value="Update">
+                    </div>
+
+                </form>
+            
+            </div>
+        </div>
+    </div>
+
+
+
 
 <div>
 
-<form action="index.php?action=updateGenre&id=<?=$genre["id_genre"]?>" method="POST">
-    
-    <h1>Update this genre</h1>
-    
-    <div class="form-input">
-        <label for="label_genre">Genre :</label>
-        <input name="label_genre" type="text"required value="<?= $genre["label_genre"] ?>">
-    </div>
 
-    <div class="button-input">
-        <input type="submit" name="updateGenre" value="Update">
-    </div>
-</form>
-</div>
 
 
 <?php
