2cda86e7b7f822fd97b50c58e4ec6af3636e63aa
M-Laetitia <molin.laetitia@gmail.com>
2023-09-23 15:22:58 +0200

    style: Add styles + improve role display

--- view/role/listRoles.php
+++ view/role/listRoles.php
@@ -1,39 +1,41 @@
 
 <?php ob_start(); ?>
 
+<div class="container-list">
+    <!-- <div class="searchBar"></div> -->
 
-<p class=""> There is/are <?= $requeteListRole->rowCount() ?> role/s </p>
+    <div class="list">
+        <table class="table-role">
+            <thead>
+                <tr>
+                    <th>Role</th>
+                    <th>Actor</th>
+                    <th>Movie</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php 
+                    foreach($requeteListRole->fetchAll() as $play) { ?>
+                        <tr>
+                            <td><a href="index.php?action=detailRole&id=<?= $play["id_role"] ?>"><?= $play["name_role"] ?></a></td>
+                            <td><a href="index.php?action=detailActeur&id=<?= $play["id_actor"] ?>"><?= $play["actorComplete"] ?></a></td>
+                            <td><a href="index.php?action=detailFilm&id=<?= $play["id_movie"]?>"><?= $play["movie_title"] ?></a></td>
+                        </tr>
+                <?php 
+                    } 
+                ?>
+            </tbody>
+        </table>
+    </div>
 
+</div>
 
-<table class="">
-    <thead>
-        <tr>
-            <th>Actor's name</th>
-            <th>Role</th>
-            <th>Movie</th>
-
-        </tr>
-    </thead>
-    <tbody>
-        <?php 
-        foreach($requeteListRole->fetchAll() as $play) { ?>
-            <tr>
-                <td><a href="index.php?action=detailActeur&id=<?= $play["id_actor"] ?>"><?= $play["actorComplete"] ?></a></td>
-                <td><a href="index.php?action=detailRole&id=<?= $play["id_role"] ?>"><?= $play["name_role"] ?></a></td>
-                <td><a href="index.php?action=detailFilm&id=<?= $play["id_movie"]?>"><?= $play["movie_title"] ?></a></td>
-            </tr>
-
-        <?php } ?>
-    </tbody>
-</table>
 
 <?php
-
 $titre = "Role";
 $titre_secondaire = "Roles List";
 $contenu = ob_get_clean();
 require "view/template.php";
-
 ?>
 
 
