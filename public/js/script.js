a2a83575dea7198e37ffe9e08e7093d08e42bd28
M-Laetitia <molin.laetitia@gmail.com>
2023-09-24 23:47:58 +0200

    feat : hamburger menu in navbar

--- public/js/script.js
+++ public/js/script.js
@@ -1,31 +1,46 @@
-// Obtenez une référence aux éléments "filmographie" et "rôle"
-const filmographyButton = document.getElementById('filmo');
-console.log("click");
-const roleButton = document.getElementById('role');
 
-// Obtenez une référence aux divs "list-role" et "list-filmo"
-const listRoleDiv = document.querySelector('.list-role');
-const listFilmoDiv = document.querySelector('.list-filmo');
+// displaying different content on the detail actor page.
 
-// Gérez le clic sur "filmographie"
-filmographyButton.addEventListener('click', function() {
-    // Affichez la filmographie et masquez le rôle
-    listRoleDiv.style.display = 'none';
-    listFilmoDiv.style.display = 'block';
+document.addEventListener('DOMContentLoaded', function() {
+    const filmographyButton = document.getElementById('filmo');
+    const roleButton = document.getElementById('role');
 
-    // Activez le bouton "filmographie" et désactivez le bouton "rôle"
-    filmographyButton.classList.add('active');
-    roleButton.classList.remove('active');
+    const listRoleDiv = document.querySelector('.list-role');
+    const listFilmoDiv = document.querySelector('.list-filmo');
 
+    filmographyButton.addEventListener('click', function() {
+        // Affichez la filmographie et masquez le rôle
+        listRoleDiv.style.display = 'none';
+        listFilmoDiv.style.display = 'block';
 
+        // Activez le bouton "filmographie" et désactivez le bouton "rôle"
+        filmographyButton.classList.add('active');
+        roleButton.classList.remove('active');
+    });
+
+    roleButton.addEventListener('click', function() {
+        // Affichez le rôle et masquez la filmographie
+        listRoleDiv.style.display = 'block';
+        listFilmoDiv.style.display = 'none';
+
+        roleButton.classList.add('active');
+        filmographyButton.classList.remove('active');
+    });
 });
 
-// Gérez le clic sur "rôle"
-roleButton.addEventListener('click', function() {
-    // Affichez le rôle et masquez la filmographie
-    listRoleDiv.style.display = 'block';
-    listFilmoDiv.style.display = 'none';
 
-    roleButton.classList.add('active');
-    filmographyButton.classList.remove('active');
+
+// Menu burger
+
+const menuBurger = document.querySelector('.menu-burger');
+console.log(menuBurger);
+const nav = document.querySelector('.primary-navigation');
+
+menuBurger.addEventListener('click', () => {
+    console.log("click");
+    menuBurger.classList.toggle("active");
+    nav.classList.toggle('active');
 });
+
+
+
