a7efe190d39ce4512076ed2bc43cc29311d9bcd9
M-Laetitia <molin.laetitia@gmail.com>
2023-09-25 23:58:08 +0200

    feat : if statement if no result

--- view/search/search.php
+++ view/search/search.php
@@ -6,15 +6,18 @@ ini_set('display_errors', 1);
 
 <h3>Résultats de la recherche :</h3>
 
-<ul>
-    <?php foreach ($results as $result) { ?>
-        <li>
-            <span>Titre du film : <?= $result["movie_title"] ?></span>
-            
-        </li>
-    <?php } ?>
-</ul>
-
+<?php if (empty($results)) { ?>
+    <p>Aucun résultat trouvé.</p>
+<?php } else { ?>
+    <ul>
+        <?php foreach ($results as $result) { ?>
+            <li>
+                <span>Titre du film : <?= $result["movie_title"] ?></span>
+                
+            </li>
+        <?php } ?>
+    </ul>
+<?php } ?>
 
 
 <?php
