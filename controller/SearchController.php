37624e9f4d45458640d547b35509db0d511567fa
M-Laetitia <molin.laetitia@gmail.com>
2023-09-25 23:53:56 +0200

    feat : WIP search feature

--- controller/SearchController.php
+++ controller/SearchController.php
@@ -2,38 +2,34 @@
 namespace Controller;
 use Model\Connect;
 error_reporting(E_ALL);
+ini_set('display_errors', 1);
 
 class SearchController {
 
     public function search() {
         $pdo = Connect::seConnecter();
         
-        // vérifier si le paramètre query est présent dans la requête post (= passé dans l'url) et s'il n'est pas vide
-        if(isset($_POST['submit-search']) && !empty($_POST['submit-search'])) {
-            $search = $_POST ['search'];
-            var_dump($search);die;
-
-            // Utilisez une requête SQL pour rechercher DB  en utilisant  LIKE et des opérateurs de correspondance partielle.
-            $requeteSearch = $pdo->prepare(
-                "SELECT * FROM movie WHERE movie.movie_title LIKE '%$search %'
-                UNION
-                SELECT * FROM person WHERE CONCAT(person.person_first_name, ' ', person.person_last_name) LIKE '%$search %'
-                UNION
-                SELECT * FROM genre WHERE genre.label_genre LIKE '%$search %'
-                UNION
-                SELECT * FROM role WHERE role.name_role LIKE '%$search %'
-                ");
-
-            
-
-            // utilisation de % :  opérateurs de correspondance partielle  pour rechercher des chaînes de caractères qui correspondent partiellement à un motif spécifié.
+        if(isset($_POST['submit-search'])) {
+            $search = $_POST['search'];
+            // var_dump($search);die;
+    
+            // Initialisez une variable pour stocker les résultats de la recherche
+            $results = [];
+    
+            // Construisez la requête SQL pour rechercher par nom de film
+            $requeteSearch = $pdo->prepare("SELECT * FROM movie WHERE movie.movie_title LIKE :search");
+    
+            // Si une soumission de recherche valide est détectée, exécutez la requête SQL
             $requeteSearch->execute(['search' => "%$search%"]);
-            // $results = $requeteSearch->fetchAll();
-            // var_dump($results); die;  
-            // var_dump($requeteSearch);
+            $results = $requeteSearch->fetchAll();
+            // var_dump($results);die;
+            
+            // Affichez les résultats de la recherche dans la vue
+            require ("view/search/search.php");
         }
-       
-        require "view/search/search.php";
+    
     }
+    
+    
 }
 
--- view/search/search.php
+++ view/search/search.php
@@ -1,30 +1,19 @@
 <?php ob_start(); 
 error_reporting(E_ALL);
+ini_set('display_errors', 1);
 ?>
 
 
-<h3>result:</h3>
+<h3>Résultats de la recherche :</h3>
 
 <ul>
-    <?php foreach ($requeteSearch->fetchAll() as $result) { 
-         ?>
-        
+    <?php foreach ($results as $result) { ?>
         <li>
-            <?php if (isset($result["movie_title"])) { ?>
-                <span>Titre du film : <?= $result["movie_title"] ?></span>
-            <?php } elseif (isset($result["person_first_name"])) { ?>
-                <span>Nom de l'acteur/réalisateur : <?= $result["person_first_name"] ?> <?= $result["person_last_name"] ?></span>
-            <?php } elseif (isset($result["label_genre"])) { ?>
-                <span>Genre : <?= $result["label_genre"] ?></span>
-            <?php } elseif (isset($result["name_role"])) { ?>
-                <span>Rôle : <?= $result["name_role"] ?></span>
-            <?php } else { ?>
-                <span>Type de résultat inconnu</span>
-            <?php } ?>
+            <span>Titre du film : <?= $result["movie_title"] ?></span>
+            
         </li>
     <?php } ?>
 </ul>
-
 
 
 
