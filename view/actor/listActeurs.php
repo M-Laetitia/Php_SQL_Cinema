8995615810fd1304a28722f96b938b411e811af3
M-Laetitia <molin.laetitia@gmail.com>
2023-09-25 22:13:41 +0200

    feat : redirection when clicking on the pic

--- view/actor/listActeurs.php
+++ view/actor/listActeurs.php
@@ -9,10 +9,20 @@
                 <div class="portrait">
                     <?php
                         if($person["person_image"] == NULL){
-                            echo '<img src="./public/Images/default_movie.jpg" alt="black and white film stock">';
+                            // echo '<img src="./public/Images/default_movie.jpg" alt="black and white film stock">';
+                            echo '<a href="index.php?action=detailActeur&id=' . $person["id_actor"] . '"><img src="./public/Images/default_movie.jpg" alt="black and white film stock"></a>';
                         }
                         else{
-                        echo "<img src=". $person["person_image"] .">";
+                            echo '<a href="index.php?action=detailActeur&id=' . $person["id_actor"] . '"><img src="' . $person["person_image"] . '"></a>';
+
+                            // Another ways to make the redirection with the hyperlink by clicking on the picture
+
+                            // echo '<a href="index.php?action=detailActeur&id=' . $person["id_actor"] . '">';
+                            // echo '<img src="' . $person["person_image"] . '">';
+                            // echo '</a>';
+                        
+                            // echo '<img src="' . $person["person_image"] . '" onclick="window.location=\'index.php?action=detailActeur&id=' . $person["id_actor"] . '\'">';
+
                         }
                     ?>
                 </div>
