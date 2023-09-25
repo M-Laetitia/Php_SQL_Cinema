0dfda520277270ea112b0073262b1bae1eb12330
M-Laetitia <molin.laetitia@gmail.com>
2023-09-26 00:22:29 +0200

    feat : search feature

--- controller/SearchController.php
+++ controller/SearchController.php
@@ -13,23 +13,54 @@ class SearchController {
             $search = $_POST['search'];
             // var_dump($search);die;
     
-            // Initialisez une variable pour stocker les résultats de la recherche
+            // initialisation variable pour stocker les résultats de la recherche
             $results = [];
     
-            // Construisez la requête SQL pour rechercher par nom de film
-            $requeteSearch = $pdo->prepare("SELECT * FROM movie WHERE movie.movie_title LIKE :search");
+            // requête SQL pour rechercher par nom de film
+            $requeteFilms = $pdo->prepare("SELECT * FROM movie WHERE movie.movie_title LIKE :search");
+
+            // requête SQL pour rechercher par nom d'acteur
+            $requeteActeurs =$pdo->prepare("SELECT * FROM actor INNER JOIN person ON actor.id_person = person.id_person WHERE person.person_first_name LIKE :search OR person.person_last_name LIKE :search");
     
-            // Si une soumission de recherche valide est détectée, exécutez la requête SQL
-            $requeteSearch->execute(['search' => "%$search%"]);
-            $results = $requeteSearch->fetchAll();
+            // requête SQL pour rechercher par nom de réalisateur
+            $requeteRealisateurs = $pdo->prepare("SELECT * FROM director INNER JOIN person ON director.id_person = person.id_person WHERE person.person_first_name LIKE :search OR person.person_last_name LIKE :search");
+
+
+            // requête SQL pour rechercher par role
+            $requeteRoles = $pdo->prepare("SELECT * FROM role WHERE role.name_role LIKE :search");
+
+            // requête SQL pour rechercher par genre
+            $requeteGenres = $pdo->prepare("SELECT * FROM genre WHERE genre.label_genre LIKE :search");
+    
+            // Exécutez toutes les requêtes SQL
+            $requeteFilms->execute(['search' => "%$search%"]);
+            $requeteActeurs->execute(['search' => "%$search%"]);
+            $requeteRealisateurs->execute(['search' => "%$search%"]);
+            $requeteGenres->execute(['search' => "%$search%"]);
+            $requeteRoles->execute(['search' => "%$search%"]);
+    
+            // Récupérez les résultats de chaque requête
+            $resultsFilms = $requeteFilms->fetchAll();
+            $resultsActeurs = $requeteActeurs->fetchAll();
+            $resultsRealisateurs = $requeteRealisateurs->fetchAll();
+            $resultsGenres = $requeteGenres->fetchAll();
+            $resultRoles = $requeteRoles->fetchAll();
+    
+            // Stockez les résultats dans un tableau associatif pour les afficher dans la vue
+            $results = [
+                'films' => $resultsFilms,
+                'acteurs' => $resultsActeurs,
+                'realisateurs' => $resultsRealisateurs,
+                'roles' => $resultRoles,
+                'genres' => $resultsGenres,
+            ];
+    
             // var_dump($results);die;
-            
-            // Affichez les résultats de la recherche dans la vue
+    
+   
             require ("view/search/search.php");
         }
-    
     }
-    
-    
+   
 }
 
--- view/search/search.php
+++ view/search/search.php
@@ -6,18 +6,80 @@ ini_set('display_errors', 1);
 
 <h3>Résultats de la recherche :</h3>
 
-<?php if (empty($results)) { ?>
+<?php if (empty($results['films']) && empty($results['acteurs']) && empty($results['realisateurs']) && empty($results['genres']) && empty($results['roles'])) { ?>
     <p>Aucun résultat trouvé.</p>
 <?php } else { ?>
-    <ul>
-        <?php foreach ($results as $result) { ?>
-            <li>
-                <span>Titre du film : <?= $result["movie_title"] ?></span>
+
+    <?php if (!empty($results['films'])) { ?>
+        <h4>Films :</h4>
+        <ul>
+            <?php foreach ($results['films'] as $film) { ?>
+                <li>
+                    <span>Titre du film : <?= $film["movie_title"] ?></span>
+                  
+                </li>
+            <?php } ?>
+        </ul>
+    <?php } ?>
+
+    <?php if (!empty($results['acteurs'])) { ?>
+        <h4>Acteurs :</h4>
+        <ul>
+            <?php foreach ($results['acteurs'] as $acteur) { ?>
+                <li>
+                    <span>Nom de l'acteur : <?= $acteur["person_first_name"] ?> <?= $acteur["person_last_name"] ?></span>
+                    
+                </li>
+            <?php } ?>
+        </ul>
+    <?php } ?>
+
+    <?php if (!empty($results['realisateurs'])) { ?>
+        <h4>Réalisateurs :</h4>
+        <ul>
+            <?php foreach ($results['realisateurs'] as $realisateur) { ?>
+                <li>
+                    <span>Nom du réalisateur : <?= $realisateur["person_first_name"] ?> <?= $realisateur["person_last_name"] ?></span>
+                  
+                </li>
+            <?php } ?>
+        </ul>
+    <?php } ?>
+
+    <?php if (!empty($results['roles'])) { ?>
+        <h4>roles :</h4>
+        <ul>
+            <?php foreach ($results['roles'] as $role) { ?>
+                <li>
+                    <span>role : <?= $role["name_role"] ?></span>
+                   
+                </li>
+            <?php } ?>
+        </ul>
+    <?php } ?>
+
+
+
+    <?php if (!empty($results['genres'])) { ?>
+        <h4>Genres :</h4>
+        <ul>
+            <?php foreach ($results['genres'] as $genre) { ?>
+                <li>
+                    <span>Genre : <?= $genre["label_genre"] ?></span>
                 
-            </li>
-        <?php } ?>
-    </ul>
+                </li>
+            <?php } ?>
+        </ul>
+    <?php } ?>
+
+
+
+
+
+
+
 <?php } ?>
+
 
 
 <?php
