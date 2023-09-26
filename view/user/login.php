c00f1e7bfda8563c76bdbddf01014db7f5585127
M-Laetitia <molin.laetitia@gmail.com>
2023-09-26 21:52:16 +0200

    feat: error messages during login

--- view/user/login.php
+++ view/user/login.php
@@ -14,18 +14,30 @@
             <input type="text" name="pseudo" id="pseudo"><br> -->
             <div class="register-input">
 
-                <label for="email">Email</label>
+                <label for="email">Email :</label>
                 <input type="text" name="email" id="email"><br>
 
-                <label for="password">Password</label>
+                <label for="password">Password :</label>
                 <input type="password" name="password" id="password"><br>
             </div>
 
-            <div id=btn-register >
+            <div id="btn-register" >
                 <input type="submit" name="submit" value="Connect">
             </div>
 
         </form>
+
+        <div>
+        <?php
+            if (isset($_SESSION["message"])) {
+                echo "<p>" . $_SESSION["message"] . "</p>";
+                unset($_SESSION["message"]); // Supprimer le message de la session
+        }?>
+        </div>
+
+        <div id="sign-up">
+            <p>Don't have an account? <a href="index.php?action=register"><span class="text-highlight">Sign up !</span></a></p>
+        </div>
 
     </div>
 
@@ -38,3 +50,5 @@ $contenu = ob_get_clean();
 require "view/template.php";
 
 ?>
+
+
