942e481abf7edf4ab8df0cefdde967ece425339c
M-Laetitia <molin.Laetitia@gmail.com>
2023-09-26 10:52:30 +0200

    feat : refine searches and results

--- controller/SearchController.php
+++ controller/SearchController.php
@@ -17,17 +17,31 @@ class SearchController {
             $results = [];
     
             // requête SQL pour rechercher par nom de film
-            $requeteFilms = $pdo->prepare("SELECT * FROM movie WHERE movie.movie_title LIKE :search");
+            $requeteFilms = $pdo->prepare("SELECT movie.* , CONCAT(person.person_first_name, ' ' ,person.person_last_name) AS directorComplete, director.id_director
+            FROM movie 
+            INNER JOIN director ON director.id_director = movie.id_director
+            INNER JOIN person ON person.id_person = director.id_person
+            WHERE movie.movie_title LIKE :search");
+
 
             // requête SQL pour rechercher par nom d'acteur
-            $requeteActeurs =$pdo->prepare("SELECT * FROM actor INNER JOIN person ON actor.id_person = person.id_person WHERE person.person_first_name LIKE :search OR person.person_last_name LIKE :search");
+            $requeteActeurs =$pdo->prepare("SELECT * 
+            FROM actor 
+            INNER JOIN person ON actor.id_person = person.id_person WHERE person.person_first_name LIKE :search OR person.person_last_name LIKE :search");
     
             // requête SQL pour rechercher par nom de réalisateur
             $requeteRealisateurs = $pdo->prepare("SELECT * FROM director INNER JOIN person ON director.id_person = person.id_person WHERE person.person_first_name LIKE :search OR person.person_last_name LIKE :search");
 
 
             // requête SQL pour rechercher par role
-            $requeteRoles = $pdo->prepare("SELECT * FROM role WHERE role.name_role LIKE :search");
+            $requeteRoles = $pdo->prepare("SELECT role.*, CONCAT(person.person_first_name, ' ' , person.person_last_name) AS acteurComplete, movie.movie_title, movie.id_movie, actor.id_actor
+            FROM role 
+            INNER JOIN play ON play.id_role = role.id_role
+            INNER JOIN actor ON actor.id_actor = play.id_actor
+            INNER JOIN person ON person.id_person = actor.id_person
+            INNER JOIN movie ON play.id_movie = movie.id_movie
+            WHERE role.name_role LIKE :search");
+
 
             // requête SQL pour rechercher par genre
             $requeteGenres = $pdo->prepare("SELECT * FROM genre WHERE genre.label_genre LIKE :search");
