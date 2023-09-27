f403d13b164fbca0c94e56a1881b5d498cd378d1
M-Laetitia <molin.Laetitia@gmail.com>
2023-09-27 16:07:08 +0200

    WIP landingpage design

--- view/landingPage/landingPage.php
+++ view/landingPage/landingPage.php
@@ -1,67 +1,13 @@
-<?php ob_start(); ?>
+<?php
+ob_start();
+ ?>
 
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-    <!-- Google font -->
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500;600;700&display=swap" rel="stylesheet">
-
-    <!-- Font awesome  -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-
-    <!-- Google icon -->
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
-
-    
-    <link rel="stylesheet" href="public/css/style-home.css">
-
-
-    <title>Home</title>
-
-
-</head>
-
-
-<body class="homepage">
-        
-   
-
-    <div class="top">
-        <!-- <img src="public/Images/background_home.jpg" alt="vintage Cinematographer’s room"> -->
-
-        <div>
-            <p>Discover</p>
-            <p>Contribute</p>
-        </div>
-        
-        <div class="icon">
-    
-            <div id="icon-discover"> </div>
-            <div id="icon-contribute"></div>
-
-        </div>
-        
-    </div>
-    <div class="bottom">
-        <div class="part">
-            <div class="square"></div>
-        </div>
-    </div>
-    
-</body>
-
-
-</html>
 
 <?php
 $titre = "Home";
-// $contenu = ob_get_clean();
-// require "view/template.php";
+$meta_description = "CineVault provides a large database of movies, actors, directors created and updated by a cinephile community ";
+$contenu = ob_get_clean();
+require "view/template.php";
 ?>
 
