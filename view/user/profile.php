720a0dabb6b9c930c9d6c1cc69282cd41ccdbc37
M-Laetitia <molin.laetitia@gmail.com>
2023-10-01 16:19:33 +0200

    style : profile informations

--- view/user/profile.php
+++ view/user/profile.php
@@ -16,29 +16,65 @@ ob_start();
         ?>
 
         <div>
-            <h1>Profil : </h1>
+            <h1><?= $infoSession["pseudo"] ?> </h1>
         </div>
 
         <div class="user-profile">
             <p><span>Username:</span> <?= $infoSession["pseudo"] ?></p>
             <p><span>Mail:</span> <?= $infoSession["email"] ?></p>
-            <p><span>Register Date:</span> <?= date("d-m-Y", strtotime($infoSession["register_date"])) ?></p>
+            <p><span>CineVault member since:</span> <?= date("d-m-Y", strtotime($infoSession["register_date"])) ?></p>
         
         </div>
 
-*
+
 
         <div class="theme-pref">
-            <form enctype="multipart/form-data" action="index.php?action=themePreference" method="POST">
-                <label for="lightTheme">Light Theme</label>
-                <input type="radio" id="lightTheme" name="theme" value="light">
-                
-                <label for="darkTheme">Dark Theme</label>
-                <input type="radio" id="darkTheme" name="theme" value="dark">
-                
+            <p id="title">Set preferred theme : </p>
+            <div class="themes">
+                <form enctype="multipart/form-data" action="index.php?action=themePreference" method="POST">
+                    <p>
+                        <label for="lightTheme">Light:</label>
+                        <input type="radio" id="lightTheme" name="theme" value="light">
+                    </p>
+
+                    <p>
+                        <label for="darkTheme">Dark:</label>
+                        <input type="radio" id="darkTheme" name="theme" value="dark">
+                    </p>
+            </div>    
                 <input type="submit" class="submit" name="submitTheme" id="submitRole" value="Update">
             </form>
         </div>
+
+        <div class="liste-notes">
+            <p>My movie ratings: </p>
+            <p><span class="text-highlight" id="toggle-list">▼</span></p>
+        </div>
+                <div class="ratings-section">
+                    
+                    <div class="liste-notes-film" id="ratings-list" style="display: none;">
+                        <?php 
+                            $notes = $requete->fetchAll();
+                            if (empty($notes)) {
+                                echo '<p>No rating here yet!</p>';
+                            } else {
+                                foreach ($notes as $note) {
+                                    ?>
+                                    <p> <span class="text-highlight" >•</span>
+
+                                    <a href="index.php?action=detailFilm&id=<?= $note["id_movie"]?>"><?= $note["movie_title"] ?></a> : 
+
+                                    <?= $note["note"]?> /5
+
+
+                                    </p>
+                                    <?php
+                                }
+                            }
+                        ?>
+                        </div>
+                </div>
+           
 
         <div id="logoutDelete">
             <p><a href="index.php?action=logout"> <span class="text-highlight"><i class="fa-solid fa-power-off"></i></span> Log out</a></p>
@@ -46,26 +82,6 @@ ob_start();
         </div>
         
 
-        <div class="ratings-section">
-            <p>My movie ratings: <span id="toggle-list">▼</span></p>
-            <div class="liste-notes-film" id="ratings-list" style="display: none;">
-                <?php 
-                    $notes = $requete->fetchAll();
-                    if (empty($notes)) {
-                        echo '<p>No rating here yet!</p>';
-                    } else {
-                        foreach ($notes as $note) {
-                            ?>
-                            <p>
-                                <?= $note["note"] ." - ". $note["movie_title"]?>
-                                <a href="#">Éditer</a>
-                            </p>
-                            <?php
-                        }
-                    }
-                ?>
-                </div>
-        </div>
     </div>
 
 
