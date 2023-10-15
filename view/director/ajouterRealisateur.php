ec8f1e2094fd6ced82630abbbf61789ef8766d11
M-Laetitia <molin.laetitia@gmail.com>
2023-10-16 01:06:44 +0200

    feat : add alert message

--- view/director/ajouterRealisateur.php
+++ view/director/ajouterRealisateur.php
@@ -21,6 +21,7 @@
                         <div class="form-input">
                             <label for="person_last_name"></label>
                             <input type="text" placeholder="Last Name*" name="person_last_name" id="person_last_name" required>
+                            <p id="personMessage" class="alertMessage"></p>
                         </div>
 
                         <div class="form-input">
